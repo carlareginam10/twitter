6cb49739f586b66250492ddeba8f6b888acdc72a
carlareginam10 <carlareginam10@gmail.com>
2019-02-20 00:39:09 -0300

    versão 0.0.2 e parte da versão 0.0.3 com contador negativo a partir de 140

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
-let texTweet = "";
 
+
+let txtTweet = document.getElementById("tweet");
+let btnEnvia = document.getElementById("btn");
+
+let txtInput = function (event) {
+btn.disabled = !event.target.value;
+}
+
+txtTweet.addEventListener("input", txtInput);
+txtTweet.dispatchEvent(new Event('input'));
+
+function showTweet () {
+    document.getElementById("msgPublish").className="hide";
+    document.getElementById("msgPublish").className="show";  
+}
+function limite_textarea(valor) {
+    quant = 140;
+    total = valor.length;
+    if(total <= quant) {
+        resto = quant - total;
+        document.getElementById('cont').innerHTML = resto;
+    if(total > 120){
+        document.getElementById('cont').innerHTML = resto;
+        resto.style.color = 'rgb(255, 0, 0)';  //não está fazendo nada
+
+    }
+    } else if ( total > quant) {
+        resto = quant - total;
+        
+        document.getElementById('cont').innerHTML = resto;  
+        //document.getElementById('msg').value = valor.substr(0,quant);
+    }
+}
+
+ function autoResize()
+    {
+        objTextArea = document.getElementById('tweet');
+        while (objTextArea.scrollHeight > objTextArea.offsetHeight)
+        {
+            objTextArea.rows += 1;
+        }
+    }
 
 
 function getTweet(){
     document.getElementById("btn").addEventListener("click", publish);
+   
 };
 
 function publish() {
@@ -16,15 +58,10 @@ function publish() {
     }else{
         document.getElementById("btn").disabled = true;
     }
-    
-    
-    
-
-   
-   
-          
-
-    
+    showTweet ();
 };
 window.onload = getTweet;
   
+
+
+
