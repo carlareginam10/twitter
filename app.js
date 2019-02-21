d3b1d2a2c3e273094f881b95d81bbdf30ebef59d
carlareginam10 <carlareginam10@gmail.com>
2019-02-21 02:18:37 -0300

    versão 0.0.4 implementado Risize e alteração no CSS

--- app.js
+++ app.js
@@ -1,80 +1,79 @@
 
-
-let txtTweet = document.getElementById("tweet");
-let btnEnvia = document.getElementById("btn");
-
-let txtInput = function (event) {
-btn.disabled = !event.target.value;
+document.getElementById("btn").addEventListener("click", publish);
+document.getElementById("tweet").addEventListener("keyup", limit_textarea);
+   
+function btnEnable(btn){
+    btn.disabled = false;
 }
 
-txtTweet.addEventListener("input", txtInput);
-txtTweet.dispatchEvent(new Event('input'));
-
+function btnDesable(btn){
+    btn.disabled = true;
+}
 
 function showTweet () {
     document.getElementById("msgPublish").className="hide";
     document.getElementById("msgPublish").className="show";  
 }
 
-function limit_textarea(value) {
+function limit_textarea() {   
+    texTweet = document.getElementById("tweet").value; 
     let quantity = 140;
-    let total = value.length;
+    let tweet = texTweet.length;
     let rest;
-    if(total <= quantity) {
-        rest = quantity - total;
-        document.getElementById('cont').innerHTML = rest;
+
+   
+    if(tweet > 140 || tweet === 0){
+         btnDesable(btn);
     }
-    if(total >= 120 ){
-        rest = quantity - total;
+    if(tweet >=1 && tweet <=140 ){
+        btnEnable(btn);        
+   }
+
+
+    if(tweet <= 140) {
+        rest = quantity - tweet;
+        document.getElementById('cont').innerHTML = rest;    
+    }
+    if(tweet >= 120 ){
+        rest = quantity - tweet;
         document.getElementById('cont').innerHTML =  rest;
         document.getElementById('cont').style.color = 'rgb(153,204,50)'
-        console.log("entrou no verde")
-    }if(total >= 130 ){
-        rest = quantity - total;
+    }
+    if(tweet >= 130 ){
+        rest = quantity - tweet;
         document.getElementById('cont').innerHTML =  rest;
         document.getElementById('cont').style.color = 'rgb(255, 0, 0)'
-        console.log("entrou no vermelho")
-      
-    } else if ( total > quantity) {
-        rest = quantity - total;        
-        document.getElementById('cont').innerHTML = rest;  
-        //document.getElementById('msg').value = valor.substr(0,quant);
     }
+   
 }
 
- function autoResize()
-    {
-        objTextArea = document.getElementById('tweet');
-        while (objTextArea.scrollHeight > objTextArea.offsetHeight)
-        {
-            objTextArea.rows += 1;
-        }
-    }
+function autoResize(tweet){
+    tweet = document.getElementById("tweet");
+    tweet.addEventListener('keydown', function(){
+    tweet.style.height = 'auto';
+    tweet.style.height = tweet.scrollHeight + 'px';
+    });
+}
 
-
-function getTweet(){
-    document.getElementById("btn").addEventListener("click", publish);
-   
-};
 
 function publish() {
+    let time = document.createElement('p');
+    let publicTime = moment().format('llll');
+    time.innerHTML = publicTime;
+    msgPublish.appendChild(time);
 
-    texTweet = document.getElementById("tweet").value;
-    
+    texTweet = document.getElementById("tweet").value; 
+    // let space = document.createElement('section');
+    // msgPublish.appendChild(space);  
+    let paragraph= document.createElement('article');
+    let txtParagraph = document.createTextNode(texTweet);
+    paragraph.appendChild(txtParagraph);
+    document.getElementById('msgPublish').appendChild(paragraph);
 
-    if(texTweet.length >= 1 && texTweet.length <=140){
-        let p= document.createElement('p');
-        let txtP = document.createTextNode(texTweet);
-        p.appendChild(txtP);
-        document.getElementById('msgPublish').appendChild(p);
-        showTweet ();    
-    }else{
-       document.getElementById("btn").disabled = true;
-    }
-    
+    showTweet ();    
 };
-window.onload = getTweet;
-  
+
+autoResize(tweet);
 
 
 
